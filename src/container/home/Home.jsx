bf2236b70624d459c8b2919b0d284ce26bc461cf
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2020-11-26 13:13:56 +0700

    LifeCycle component

--- src/container/home/Home.jsx
+++ src/container/home/Home.jsx
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
-import Product from '../../component/Product/Product';
+// import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
+// import Product from '../../component/Product/Product';
+import LifeCycleComp from '../../component/LifeCycleComp/LifeCycleComp';
 
 class Home extends Component {
+    state = {
+        showComponent: true
+    }
+
+    componentDidMount (){
+        setTimeout (() => {
+            this.setState({
+                showComponent: false
+            })
+        }, 15000)
+    }
     render() {
         return (
             <div>
-            <p>Youtube Component</p>
-            <hr/>
+            {/* <p>Youtube Component</p>
+            <hr/> */}
                 {/* <YouTubeComp time="7.12" title="Belajar react js bagian 1" desc="1 jt kali ditonton. 1 minggu yang lalu"/>
                 <YouTubeComp time="8.20" title="Belajar react js bagian 2" desc="2 jt kali ditonton. 2 minggu yang lalu"/>
                 <YouTubeComp time="6.40" title="Belajar react js bagian 3" desc="3 jt kali ditonton. 3 minggu yang lalu"/>
                 <YouTubeComp time="10.01" title="Belajar react js bagian 4" desc="4 jt kali ditonton. 4 minggu yang lalu"/>
                 <YouTubeComp/> */}
-                <p>Counter</p>
+                {/* <p>Counter</p>
                 <hr/>
-                <Product/>
+                <Product/> */}
+                <p>LifeCycle Component</p>
+                <hr/>
+                {
+                     //jika statecomponent bernilai true maka munculkan LifeCycleComp,jika statecomponent bernilai false maka tidak memunculkan komponen apapun.
+                    this.state.showComponent ?
+                    <LifeCycleComp/> : null
+                }
             </div>
         )
     }
